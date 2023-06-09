e602fa6b264fbf683c8f9d42401a8fd02187e245
RitsuYuzaki  ( 由崎律 ) <me@ritsu-me.com>
2023-06-09 12:31:19 +0900

    tintin

--- src/lib/config/icon.ts
+++ src/lib/config/icon.ts
@@ -2,7 +2,7 @@ import type { Icon } from '$lib/types/icon'
 import { site } from '$lib/config/site'
 
 export const favicon: Icon = {
-  src: site.protocol + site.domain + "/favicon.png",
+  src: site.protocol + site.domain + "/asetts/Zone.png",
   sizes: "500x500",
   type: "image/png",
 };
@@ -27,12 +27,12 @@ export const any: { [key: number]: Icon } = {
 
 export const maskable: { [key: number]: Icon } = {
   192: {
-    src: site.protocol + site.domain + '/assets/ritsu.png',
+    src: site.protocol + site.domain + '/assets/Zone.png',
     sizes: '192x192',
     type: 'image/png'
   },
   512: {
-    src: site.protocol + site.domain + '/assets/ritsu.png',
+    src: site.protocol + site.domain + '/assets/Zone.png',
     sizes: '512x512',
     type: 'image/png'
   }
