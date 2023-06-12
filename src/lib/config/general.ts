1391a8831003a00cb82149737e717484905297b5
RitsuYuzaki  ( 由崎律 ) <me@ritsu-me.com>
2023-06-12 11:55:14 +0900

    tntn

--- src/lib/config/general.ts
+++ src/lib/config/general.ts
@@ -56,12 +56,8 @@ export const head: HeadConfig = {}
 export const header: HeaderConfig = {
   nav: [
     {
-      text: 'Get Started',
+      text: 'Introduction',
       link: '/hello-world'
-    },
-    {
-      text: 'Elements',
-      link: '/hello-world/elements'
     }
   ]
 }
